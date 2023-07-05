9682f3c36c659693fad7cab52b71984169b9b5db
Mihail Nechaev <sadovni4ek@gmail.com>
2023-07-05 19:47:25 +0300

    DOM variables

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Swiper Js
+
+// Nav open close
+
+const body = document.querySelector("body");
+const navMenu = body.querySelector(".menu-content");
+const navClosebtn = navMenu.querySelector(".navClose-btn")
+
+// Change bg color
+
+// Scroll up button
+
+// Nav link indicator
+
+// Scroll reveal Animation
