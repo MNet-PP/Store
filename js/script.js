2d2af6bc66899ef93a7ca4ee6532dd3c1495d653
Mihail Nechaev <sadovni4ek@gmail.com>
2023-07-05 22:09:09 +0300

    swiper object

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
 // Swiper Js
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 // Nav open close
 
 const body = document.querySelector("body");
 const navMenu = body.querySelector(".menu-content");
-const navClosebtn = navMenu.querySelector(".navClose-btn")
+const navClosebtn = navMenu.querySelector(".navClose-btn");
+const navOpenbtn = body.querySelector(".navOpen-btn");
+
+if (navMenu && navOpenbtn) {
+    navOpenbtn.addEventListener("click", () => {
+        navMenu.classList.add("open");
+        body.style.overflowY = "hidden";
+    })
+}
+
+if (navMenu && navClosebtn) {
+    navClosebtn.addEventListener("click", () => {
+        navMenu.classList.remove("open");
+        body.style.overflowY = "scroll";
+    })
+}
 
 // Change bg color
 
