7d23bb767ff7d8234f1b3bca2a29f33b0d1506b8
Mihail Nechaev <sadovni4ek@gmail.com>
2023-07-06 19:09:05 +0300

    JavaScript for slides

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // Swiper Js
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  //grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 // Nav open close
 
 const body = document.querySelector("body");
